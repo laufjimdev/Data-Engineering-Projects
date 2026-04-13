7a6c910260b90f01a03edcc1e0f1f53c30bc83c1
Laura Jimenez <laufjimdev@gmail.com>
2026-04-13 13:50:20 -0400

    03weather - changed the concept of the project, now project is meant to extract temps prediction data from API and come back at specific hours to take current temps measurements and calculate the prediction error. Created README file. Started to load data to db.

---
+++ 03bash_project_weather_info_etl/README.md
@@ -0,0 +1,6 @@
+This is my first Bash scripting ETL Project, with this project my intentions are to practice the knoledge acquired during my learning journey, through trial and error practices.
+
+I am creating a script that extracts weather data from the Weather API: wttr.in/
+This project intens to check the weather predictions from this API and compare them to the actual weather measurements in real time.
+
+This was my first time handling json files, I played around fetching the data and sending it to files for visibility and comprehension of the data.
--- 03bash_project_weather_info_etl/weather_etl_script.sh
+++ 03bash_project_weather_info_etl/weather_etl_script.sh
@@ -1,13 +1,44 @@
 #! /bin/bash
 
 #### EXTRACTION ####
-#Downloading the data to a json file
-curl -s "wttr.in/?format=j1" > weather.json
-#Parsing the data
-temp=$(jq -r '.current_condition[0].temp_F' weather.json) 
-uv=$(jq -r '.current_condition[0].uvIndex' weather.json)
-precip=$(jq -r '.current_condition[0].precipMM' weather.json)
-humidity=$(jq -r '.current_condition[0].humidity' weather.json)
-sky=$(jq -r '.current_condition[0].weatherDesc[0].value' weather.json)
 
-echo $sky
+loc_1="New+York"
+#local_time=$(curl -s "wttr.in/new+york?format=%T" | cut -c1-2)
+#if [[ $local_time == 0 ]]
+#
+curl -s "wttr.in/$loc_1?format=j1" > weather_new_york.json
+#fi
+
+#New York
+morning_temp=$(jq -r '.weather[0].hourly[2].tempF' weather_new_york.json)
+noon_temp=$(jq -r '.weather[0].hourly[4].tempF' weather_new_york.json)
+evening_temp=$(jq -r '.weather[0].hourly[6].tempF' weather_new_york.json)
+night_temp=$(jq -r '.weather[0].hourly[7].tempF' weather_new_york.json)
+
+time_stamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+#### LOAD ####
+DB='/Users/lauradev/Desktop/Data Engineering Projects/03bash_project_weather_info_etl/weather.db'
+
+sqlite3 $DB << EOF
+CREATE TABLE IF NOT EXISTS weather_predictions (
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    time_stamp  DATETIME,
+    morning_temp_f REAL,
+    noon_temp_f   REAL,
+    evening_temp_f REAL,
+    night_temp_f  REAL
+);
+
+INSERT INTO weather_predictions (time_stamp, morning_temp_f, noon_temp_f, evening_temp_f, night_temp_f)
+VALUES ('$time_stamp', '$morning_temp', '$noon_temp', '$evening_temp', '$night_temp');
+EOF
+
+echo "Inserted: $time_stamp | morning: $morning_temp | noon: $noon_temp | evening: $evening_temp | night: $night_temp"
+
+#Parsing the data
+curr_temp=$(jq -r '.current_condition[0].temp_F' weather_new_york.json) 
+
+
+
+
